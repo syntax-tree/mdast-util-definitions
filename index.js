eed262c7a66f88408319e3f728aa9cd60dce70ad
Titus Wormer <tituswormer@gmail.com>
2016-11-13 14:33:20 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,92 +1,40 @@
-/**
- * @author Titus Wormer
- * @copyright 2015-2016 Titus Wormer. All rights reserved.
- * @license MIT
- * @module mdast:util:definitions
- * @fileoverview Get a definition in `node` by `identifier`.
- */
-
 'use strict';
 
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var visit = require('unist-util-visit');
 
-/**
- * Factory to get a node from the given definition-cache.
- *
- * @private
- * @param {Object.<string, Node>} cache - Definitions.
- * @return {function(string): Node?} - Getter, bound to
- *   `cache`.
- */
-function getterFactory(cache) {
-    /**
-     * Get a node from the bound definition-cache.
-     *
-     * @private
-     * @param {string} identifier - Identifier of
-     *   definition.
-     * @return {Node?} - Definition, if found.
-     */
-    function getter(identifier) {
-        return (identifier && cache[identifier.toUpperCase()]) || null;
-    }
-
-    return getter;
-}
-
-/**
- * Gather all definitions in `node`
- *
- * @private
- * @param {Node} node - (Grand)parent of definitions.
- * @return {Object.<string, Node>} - Map of found
- *   definitions by their identifier.
- */
-function gather(node) {
-    var cache = {};
-
-    if (!node || !node.type) {
-        throw new Error('mdast-util-definitions expected node');
-    }
-
-    /**
-     * Add `definition` to `cache` if it has an identifier.
-     *
-     * @param {Node} definition - Definition node.
-     */
-    function check(definition) {
-        cache[definition.identifier.toUpperCase()] = definition;
-    }
-
-    visit(node, 'definition', check);
-
-    return cache;
-}
-
-/**
- * Get a definition in `node` by `identifier`.
- *
- * Supports weird keys (like `__proto__`).
- *
- * @example
- *   var ast = remark.parse('[example]: http://example.com "Example"');
- *   var getDefinition = getDefinitionFactory(ast);
- *   getDefinition('example');
- *   // {type: 'definition', 'title': 'Example', ...}
- *
- * @param {Node} node - (Grand)parent of definitions.
- * @return {function(string): Node?} - Getter.
- */
-function getDefinitionFactory(node) {
-    return getterFactory(gather(node));
-}
-
-/*
- * Expose
- */
-
+/* Expose. */
 module.exports = getDefinitionFactory;
+
+/* Get a definition in `node` by `identifier`. */
+function getDefinitionFactory(node) {
+  return getterFactory(gather(node));
+}
+
+/* Gather all definitions in `node` */
+function gather(node) {
+  var cache = {};
+
+  if (!node || !node.type) {
+    throw new Error('mdast-util-definitions expected node');
+  }
+
+  visit(node, 'definition', check);
+
+  return cache;
+
+  /* Add `definition` to `cache` if it has an identifier.   */
+  function check(definition) {
+    cache[definition.identifier.toUpperCase()] = definition;
+  }
+}
+
+/* Factory to get a node from the given definition-cache. */
+function getterFactory(cache) {
+  return getter;
+
+  /* Get a node from the bound definition-cache. */
+  function getter(identifier) {
+    return (identifier && cache[identifier.toUpperCase()]) || null;
+  }
+}
--- test.js
+++ test.js
@@ -1,96 +1,70 @@
 'use strict';
 
-/* eslint-env node */
-
-/*
- * Dependencies.
- */
-
 var test = require('tape');
 var remark = require('remark');
 var definitions = require('./index.js');
 
-/*
- * Tests.
- */
-
 test('mdast-util-definitions', function (t) {
-    var getDefinition;
-    var ast;
+  var getDefinition;
+  var tree;
 
-    t.throws(
-        function () {
-            definitions();
-        },
-        /mdast-util-definitions expected node/,
-        'should fail without node'
-    );
+  t.throws(
+    function () {
+      definitions();
+    },
+    /mdast-util-definitions expected node/,
+    'should fail without node'
+  );
 
-    ast = remark().parse('[example]: http://example.com "Example"');
-    getDefinition = definitions(ast);
+  tree = remark().parse('[example]: http://example.com "Example"');
+  getDefinition = definitions(tree);
 
-    t.deepEqual(
-        getDefinition('example'),
-        {
-            'type': 'definition',
-            'identifier': 'example',
-            'url': 'http://example.com',
-            'title': 'Example',
-            'position': {
-                'start': {
-                    'column': 1,
-                    'line': 1,
-                    'offset': 0
-                },
-                'end': {
-                    'column': 40,
-                    'line': 1,
-                    'offset': 39
-                },
-                'indent': []
-            }
-        },
-        'should return a definition'
-    );
+  t.deepEqual(
+    getDefinition('example'),
+    {
+      type: 'definition',
+      identifier: 'example',
+      url: 'http://example.com',
+      title: 'Example',
+      position: {
+        start: {column: 1, line: 1, offset: 0},
+        end: {column: 40, line: 1, offset: 39},
+        indent: []
+      }
+    },
+    'should return a definition'
+  );
 
-    t.equal(
-        getDefinition('foo'),
-        null,
-        'should return null when not found'
-    );
+  t.equal(
+    getDefinition('foo'),
+    null,
+    'should return null when not found'
+  );
 
-    ast = remark().parse('[__proto__]: http://proto.com "Proto"');
-    getDefinition = definitions(ast);
+  tree = remark().parse('[__proto__]: http://proto.com "Proto"');
+  getDefinition = definitions(tree);
 
-    t.deepEqual(
-        getDefinition('__proto__'),
-        {
-            'type': 'definition',
-            'identifier': '__proto__',
-            'url': 'http://proto.com',
-            'title': 'Proto',
-            'position': {
-                'start': {
-                    'column': 1,
-                    'line': 1,
-                    'offset': 0
-                },
-                'end': {
-                    'column': 38,
-                    'line': 1,
-                    'offset': 37
-                },
-                'indent': []
-            }
-        },
-        'should work on weird identifiers'
-    );
+  t.deepEqual(
+    getDefinition('__proto__'),
+    {
+      type: 'definition',
+      identifier: '__proto__',
+      url: 'http://proto.com',
+      title: 'Proto',
+      position: {
+        start: {column: 1, line: 1, offset: 0},
+        end: {column: 38, line: 1, offset: 37},
+        indent: []
+      }
+    },
+    'should work on weird identifiers'
+  );
 
-    t.deepEqual(
-        getDefinition('toString'),
-        null,
-        'should work on weird identifiers when not found'
-    );
+  t.deepEqual(
+    getDefinition('toString'),
+    null,
+    'should work on weird identifiers when not found'
+  );
 
-    t.end();
+  t.end();
 });
