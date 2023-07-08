9e02a5b2c72019770205d1ee32b60ad19c4e4dc0
Titus Wormer <tituswormer@gmail.com>
2023-07-08 11:54:35 +0200

    Change to use `export` map

--- test.js
+++ test.js
@@ -4,14 +4,15 @@
 
 import assert from 'node:assert/strict'
 import test from 'node:test'
+import {definitions} from 'mdast-util-definitions'
 import {fromMarkdown} from 'mdast-util-from-markdown'
-import {definitions} from './index.js'
 
 test('definitions', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
-      'definitions'
-    ])
+    assert.deepEqual(
+      Object.keys(await import('mdast-util-definitions')).sort(),
+      ['definitions']
+    )
   })
 
   await t.test('should fail without node', async function () {
